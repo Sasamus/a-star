1c66f96c82ae43cc7b26fe6cb7d83cb10827aaea
Sasamus <labus93@gmail.com>
2015-02-23 16:59:27 +0100

    Implemented and structured most of the A* algorithm, it shouldn't work propperly but the framework is done

--- src/se/miun/dt015a/astar/romania/TouringRomania.java
+++ src/se/miun/dt015a/astar/romania/TouringRomania.java
@@ -35,26 +35,106 @@ public class TouringRomania implements InformedSearch<City, DriveAction> {
 		// InformedSearch<City,DriveAction> randomSearch = new RandomSearch<>();
 		// return randomSearch.search(problem, heuristic);
 		// /* ----- You should remove this ----- */
-		
-		// A Comparator class
-	    final Comparator<City> comparator = new Comparator<City>(){
-	         
-	        @Override
-	        public int compare(City c1, City c2) {
-	            return (int) (heuristic.apply(c1) - heuristic.apply(c2));
-	        }
-	    };
-		
-		
-		
+
+		// A Comparator
+		final Comparator<City> comparator = new Comparator<City>() {
+
+			@Override
+			public int compare(City c1, City c2) {
+
+				// TODO: Add g(n) to comparator
+
+				return (int) (heuristic.apply(c1) - heuristic.apply(c2));
+			}
+		};
+
+		// A node to hold the current state, currently at the start state
 		City node = problem.getStart();
+
+		// Hold the nodes int he frontier
 		PriorityQueue<City> frontier = new PriorityQueue<City>(comparator);
+
+		// A HashSet to hold the explored nodes
 		HashSet<City> explored = new HashSet<City>();
 
-		
-		
-		
+		// Holds the successors
+		HashSet<Successor<City, DriveAction>> successors;
 
+		// Add node to frontier
+		frontier.add(node);
+
+		// Loop forever
+		while (true) {
+
+			// Check if frontier is empty
+			if (frontier.isEmpty()) {
+
+				// Return null
+				return null;
+			}
+
+			// Get the next node from frontier
+			node = frontier.poll();
+
+			// Check if node equals the goal state
+			if (problem.isGoal(node)) {
+
+				// TODO: Return solution here
+			}
+
+			// Add node to explored
+			explored.add(node);
+
+			// Get Successors to node
+			successors = (HashSet<Successor<City, DriveAction>>) problem
+					.getSuccessors(node);
+
+			// Holds the Node to be removed
+			City replaceNode = null;
+
+			// Iterate through the successors in successors
+			for (Successor<City, DriveAction> successor : successors) {
+
+				// Check if an successors state exists in explored or frontier
+				if (!explored.contains(successor.state)
+						&& !frontier.contains(successor.state)) {
+
+					// If it doesn't, insert successors state in frontier
+					frontier.add(successor.state);
+
+				}
+				// If frontier contains successors.state
+				else if (frontier.contains(successor.state)) {
+
+					// Iterate through frontier
+					for (City tmpNode : frontier) {
+
+						// If tmpNode equals successor.state
+						if (tmpNode.equals(successor.state)) {
+
+							// Check if sucessor.state is better according to
+							// comparator
+							if (0 > comparator
+									.compare(tmpNode, successor.state)) {
+
+								// If it is, set replaceNode to tmpNode
+								replaceNode = tmpNode;
+							}
+						}
+					}
+				}
+
+				// Check if replaceNode have been set
+				if (replaceNode != null) {
+
+					// Remove replaceNode from frontier
+					frontier.remove(replaceNode);
+
+					// Add successor.state to frontier
+					frontier.add(successor.state);
+				}
+			}
+		}
 	}
 
 	public static void main(String[] args) {
